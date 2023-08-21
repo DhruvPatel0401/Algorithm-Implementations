9142f0dcb2cfaf77f541a429dad1c7e4920eee35
Dhruv Patel <pateldhruv130401@gmail.com>
2023-08-21 10:29:11 +0530

    Queue Implementation

---
+++ Data Structures/Queue.py
@@ -0,0 +1,21 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if self.queue:
+            return self.queue.popleft()
+        else:
+            return None
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
